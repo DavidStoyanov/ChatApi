b07b563948c423b127388f15b23504aed2b12134
DavidStoyanov <david_st1@abv.bg>
2025-08-12 12:11:13 +0300

    Init

---
+++ src/test/java/org/myapp/chat_api/MyChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myapp.chat_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
